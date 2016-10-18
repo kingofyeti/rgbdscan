524a3768016c359adc02fe6daefcbfa210105654
Chao Wang <chaowang15@gmail.com>
2016-10-18 18:00:45 -0500

    modify details

--- matterportscan/openni2interface.cpp
+++ matterportscan/openni2interface.cpp
@@ -148,27 +148,27 @@ void OpenNI2Interface::initKinectDevice()
 					{
 						//if (i == 0) // only set depth/color width and height once
 						//{
-							openni::VideoMode depthVideoMode = m_depthStream[i].getVideoMode();
-							openni::VideoMode colorVideoMode = m_colorStream[i].getVideoMode();
-							m_depthWidth = depthVideoMode.getResolutionX();
-							m_depthHeight = depthVideoMode.getResolutionY();
-							m_colorWidth = colorVideoMode.getResolutionX();
-							m_colorHeight = colorVideoMode.getResolutionY();
-							cout << "Depth = (" << m_depthWidth << "," << m_depthHeight << ")" << endl;
-							cout << "Color = (" << m_colorWidth << "," << m_colorHeight << ")" << endl;
+						openni::VideoMode depthVideoMode = m_depthStream[i].getVideoMode();
+						openni::VideoMode colorVideoMode = m_colorStream[i].getVideoMode();
+						m_depthWidth = depthVideoMode.getResolutionX();
+						m_depthHeight = depthVideoMode.getResolutionY();
+						m_colorWidth = colorVideoMode.getResolutionX();
+						m_colorHeight = colorVideoMode.getResolutionY();
+						cout << "Depth = (" << m_depthWidth << "," << m_depthHeight << ")" << endl;
+						cout << "Color = (" << m_colorWidth << "," << m_colorHeight << ")" << endl;
 
-							//if (depthWidth == colorWidth &&
-							//	depthHeight == colorHeight)
-							//{
-							//	m_width = depthWidth;
-							//	m_height = depthHeight;
-							//}
-							//else
-							//{
-							//	cout << "ERROR: expect color and depth to be in same resolution:" << endl;
-							//	openni::OpenNI::shutdown();
-							//	m_flagInitSuccessful = false;
-							//}
+						//if (depthWidth == colorWidth &&
+						//	depthHeight == colorHeight)
+						//{
+						//	m_width = depthWidth;
+						//	m_height = depthHeight;
+						//}
+						//else
+						//{
+						//	cout << "ERROR: expect color and depth to be in same resolution:" << endl;
+						//	openni::OpenNI::shutdown();
+						//	m_flagInitSuccessful = false;
+						//}
 						//}
 
 						m_colorStream[i].getCameraSettings()->setAutoWhiteBalanceEnabled(true);
@@ -176,7 +176,7 @@ void OpenNI2Interface::initKinectDevice()
 						//float delta = 100;
 						//m_colorStream[i].getCameraSettings()->setExposure(delta);
 						//m_colorStream[i].getCameraSettings()->setAutoExposureEnabled(false);
-						
+
 						m_flagInitSuccessful = true;
 					}
 				}
@@ -324,121 +324,132 @@ void OpenNI2Interface::logData(int32_t * timestamp,
 
 void OpenNI2Interface::decompressLog(char* filename)
 {
-	string outputFolder("./saved/");
-	wstring wstr(outputFolder.begin(), outputFolder.end());
-	if (CreateDirectory(wstr.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError())
+	string currentFolder("./");
+	wstring wstrCurrent(currentFolder.begin(), currentFolder.end());
+	if (CreateDirectory(wstrCurrent.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError())
 	{
-		string outputFolderColor = outputFolder + "rgb/";
-		wstring wstrColor(outputFolderColor.begin(), outputFolderColor.end());
-		if (CreateDirectory(wstrColor.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError())
+		string outputFolder = outputFolder + "saved/";
+		wstring wstr(outputFolder.begin(), outputFolder.end());
+		if (CreateDirectory(wstr.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError())
 		{
-			string outputFolderDepth = outputFolder + "depth/";
-			wstring wstrDepth(outputFolderDepth.begin(), outputFolderDepth.end());
-			if (CreateDirectory(wstrDepth.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError())
+			string outputFolderColor = outputFolder + "rgb/";
+			wstring wstrColor(outputFolderColor.begin(), outputFolderColor.end());
+			if (CreateDirectory(wstrColor.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError())
 			{
-				string strDepthWindowName("Depth"), strColorWindowName("Color");
-				if (m_flagShowImage)
+				string outputFolderDepth = outputFolder + "depth/";
+				wstring wstrDepth(outputFolderDepth.begin(), outputFolderDepth.end());
+				if (CreateDirectory(wstrDepth.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError())
 				{
-					cv::namedWindow( strDepthWindowName,  CV_WINDOW_AUTOSIZE );
-					cv::namedWindow( strColorWindowName,  CV_WINDOW_AUTOSIZE );
-				}
-				int numFrames = 0, numDevices = 0, frameIdx = 0, depthSize = 0, rgbSize = 0;
-				logFile = fopen(filename, "rb");
-				if (logFile)
-				{
-					fread(&numFrames, sizeof(int32_t), 1, logFile);
-					fread(&numDevices, sizeof(int32_t), 1, logFile);
-					fread(&m_depthWidth, sizeof(int32_t), 1, logFile);
-					fread(&m_depthHeight, sizeof(int32_t), 1, logFile);
-					fread(&m_colorWidth, sizeof(int32_t), 1, logFile);
-					fread(&m_colorHeight, sizeof(int32_t), 1, logFile);
-					unsigned char *rgbData = new unsigned char[m_colorWidth * m_colorHeight];
-					unsigned char *depthData = new unsigned char[m_depthWidth * m_depthHeight * 2];
-					int numDigits = getNumberOfDigits(numFrames);
-					for (int i = 0; i < numFrames; ++i)
+					string strDepthWindowName("Depth"), strColorWindowName("Color");
+					if (m_flagShowImage)
 					{
-						for (int j = 0; j < numDevices; ++j)
+						cv::namedWindow( strDepthWindowName,  CV_WINDOW_AUTOSIZE );
+						cv::namedWindow( strColorWindowName,  CV_WINDOW_AUTOSIZE );
+					}
+					int numFrames = 0, numDevices = 0, frameIdx = 0, depthSize = 0, rgbSize = 0;
+					logFile = fopen(filename, "rb");
+					if (logFile)
+					{
+						fread(&numFrames, sizeof(int32_t), 1, logFile);
+						fread(&numDevices, sizeof(int32_t), 1, logFile);
+						fread(&m_depthWidth, sizeof(int32_t), 1, logFile);
+						fread(&m_depthHeight, sizeof(int32_t), 1, logFile);
+						fread(&m_colorWidth, sizeof(int32_t), 1, logFile);
+						fread(&m_colorHeight, sizeof(int32_t), 1, logFile);
+						unsigned char *rgbData = new unsigned char[m_colorWidth * m_colorHeight];
+						unsigned char *depthData = new unsigned char[m_depthWidth * m_depthHeight * 2];
+						int numDigits = getNumberOfDigits(numFrames);
+						for (int i = 0; i < numFrames; ++i)
 						{
-							fread(&frameIdx, sizeof(int32_t), 1, logFile);
-							fread(&depthSize, sizeof(int32_t), 1, logFile);
-							fread(&rgbSize, sizeof(int32_t), 1, logFile);
+							for (int j = 0; j < numDevices; ++j)
+							{
+								fread(&frameIdx, sizeof(int32_t), 1, logFile);
+								fread(&depthSize, sizeof(int32_t), 1, logFile);
+								fread(&rgbSize, sizeof(int32_t), 1, logFile);
 
-							memset(depthData, 0, m_depthWidth * m_depthHeight * 2);
-							// Decompress depth image
-							unsigned char *depthDataBinary = new unsigned char[depthSize];
-							fread(depthDataBinary, depthSize, 1, logFile);
-							unsigned long len = (unsigned long)(m_depthWidth * m_depthHeight * 2);
-							//unsigned long len = 0;
-							//unsigned long len = 1000000;
-							int res = uncompress(depthData, &len, depthDataBinary, (unsigned long)depthSize);
-							delete []depthDataBinary;
-							//if (res != 0)
-							//{
-							//	cout << "-------------------------------------" << endl;
-							//}
-							depthDataBinary = NULL;
-							cv::Mat mImageDepth(m_depthHeight, m_depthWidth, CV_16UC1, (void *)depthData);
-							cv::Mat mScaledDepth;
-							mImageDepth.convertTo( mScaledDepth, CV_16UC1, cst_depthScaleFactor );
-							if (m_flagScaleImage)
-							{
-								cv::Size dstSize;
-								dstSize.width = m_colorWidth;
-								dstSize.height = m_colorHeight;
-								cv::resize(mScaledDepth, mScaledDepth, dstSize);
-							}
-							cv::flip(mScaledDepth, mScaledDepth, 1);
+								memset(depthData, 0, m_depthWidth * m_depthHeight * 2);
+								// Decompress depth image
+								unsigned char *depthDataBinary = new unsigned char[depthSize];
+								fread(depthDataBinary, depthSize, 1, logFile);
+								unsigned long len = (unsigned long)(m_depthWidth * m_depthHeight * 2);
+								//unsigned long len = 0;
+								//unsigned long len = 1000000;
+								int res = uncompress(depthData, &len, depthDataBinary, (unsigned long)depthSize);
+								delete []depthDataBinary;
+								//if (res != 0)
+								//{
+								//	cout << "-------------------------------------" << endl;
+								//}
+								depthDataBinary = NULL;
+								cv::Mat mImageDepth(m_depthHeight, m_depthWidth, CV_16UC1, (void *)depthData);
+								cv::Mat mScaledDepth;
+								mImageDepth.convertTo( mScaledDepth, CV_16UC1, cst_depthScaleFactor );
+								if (m_flagScaleImage)
+								{
+									cv::Size dstSize;
+									dstSize.width = m_colorWidth;
+									dstSize.height = m_colorHeight;
+									cv::resize(mScaledDepth, mScaledDepth, dstSize);
+								}
+								cv::flip(mScaledDepth, mScaledDepth, 1);
 
-							string depthImageName(outputFolderDepth);
-							string strName;
-							for (int k = 0; k != numDigits; ++k)
-							{
-								strName += "0";
-							}
-							string temp = to_string((long long)i);
-							int val = temp.length();
-							strName.replace(numDigits - val, val, temp);
-							strName = to_string((long long)j) + "_" + strName + ".png";
-							depthImageName += strName;
-							cv::imwrite(depthImageName.c_str(), mScaledDepth);
-							if (m_flagShowImage && j == 0)
-							{
-								cv::imshow(strDepthWindowName, mScaledDepth );
-							}
-							cout << "Frame " << i <<", Device " << j << "..." << endl;
-							//cout << "  depthSize = " << depthSize << endl;
+								string depthImageName(outputFolderDepth);
+								string strName;
+								for (int k = 0; k != numDigits; ++k)
+								{
+									strName += "0";
+								}
+								string temp = to_string((long long)i);
+								int val = temp.length();
+								strName.replace(numDigits - val, val, temp);
+								strName = to_string((long long)j) + "_" + strName + ".png";
+								depthImageName += strName;
+								cv::imwrite(depthImageName.c_str(), mScaledDepth);
+								if (m_flagShowImage && j == 0)
+								{
+									cv::imshow(strDepthWindowName, mScaledDepth );
+								}
+								cout << "Frame " << i <<", Device " << j << "..." << endl;
+								//cout << "  depthSize = " << depthSize << endl;
 
-							// Decompress color image
-							fread(rgbData, rgbSize, 1, logFile);
-							CvMat mat = cvMat(m_colorHeight, m_colorWidth, CV_8UC3, rgbData);
-							//IplImage *p = cvDecodeImage( &mat, 1 );
-							CvMat *p = cvDecodeImageM(&mat, 1);
-							cv::Mat m(p);
-							cv::cvtColor(m, m, CV_BGR2RGB);
+								// Decompress color image
+								fread(rgbData, rgbSize, 1, logFile);
+								CvMat mat = cvMat(m_colorHeight, m_colorWidth, CV_8UC3, rgbData);
+								//IplImage *p = cvDecodeImage( &mat, 1 );
+								CvMat *p = cvDecodeImageM(&mat, 1);
+								cv::Mat m(p);
+								cv::cvtColor(m, m, CV_BGR2RGB);
 
-							cv::flip(m, m, 1);
-							string rgbImageName(outputFolderColor);
-							rgbImageName += strName;
-							imwrite(rgbImageName.c_str(), m);
-							if (m_flagShowImage && j == 0)
-							{
-								cv::imshow(strColorWindowName, m );
-							}
-							if(m_flagShowImage && cv::waitKey( 1 ) == 27) // esc to quit
-							{
-								break;
+								cv::flip(m, m, 1);
+								string rgbImageName(outputFolderColor);
+								rgbImageName += strName;
+								imwrite(rgbImageName.c_str(), m);
+								if (m_flagShowImage && j == 0)
+								{
+									cv::imshow(strColorWindowName, m );
+								}
+								if(m_flagShowImage && cv::waitKey( 1 ) == 27) // esc to quit
+								{
+									break;
+								}
 							}
 						}
+						fclose(logFile);
+						delete []rgbData;
+						delete []depthData;
 					}
-					fclose(logFile);
-					delete []rgbData;
-					delete []depthData;
-				}
-				else
-				{
-					cerr << "WARNING: cannot open the file " << filename << endl;
+					else
+					{
+						cerr << "WARNING: cannot open the file " << filename << endl;
+					}
 				}
 			}
+		}
+		else
+		{
+			// Failed to create directory.
+			cout << "WARNING: cannot create the directory for saving decompressed images." << endl;
+			return;
 		}
 	}
 	else
@@ -447,5 +458,4 @@ void OpenNI2Interface::decompressLog(char* filename)
 		cout << "WARNING: cannot create the directory for saving decompressed images." << endl;
 		return;
 	}
-
 }
